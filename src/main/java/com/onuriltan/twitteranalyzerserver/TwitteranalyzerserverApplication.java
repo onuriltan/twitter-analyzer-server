987cf82cd1ea9f48db254c8e9c3ba9f4c1e997a4
onur.iltan <onur.iltan@altpro.com.tr>
2018-03-12 13:59:38 +0300

    initial commit

---
+++ src/main/java/com/onuriltan/twitteranalyzerserver/TwitteranalyzerserverApplication.java
@@ -0,0 +1,12 @@
+package com.onuriltan.twitteranalyzerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitteranalyzerserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitteranalyzerserverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/onuriltan/twitteranalyzerserver/TwitteranalyzerserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.onuriltan.twitteranalyzerserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitteranalyzerserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
