5627aef4633dccb8f38440dfac287658f68e9f3c
onuriltan <onur.iltan@altpro.com.tr>
2018-04-24 00:18:21 +0300

    configs

--- src/main/java/com/onuriltan/twitteranalyzerserver/api/trendtopic/controller/TrendTopicController.java
+++ src/main/java/com/onuriltan/twitteranalyzerserver/api/trendtopic/controller/TrendTopicController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.inject.Inject;
 
 @RestController
-@RequestMapping("/twitteranalyzer/api")
+@RequestMapping("/api")
 public class TrendTopicController {
 
     @Inject
