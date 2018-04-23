4b2aacecef406e3bcfb16bb14f80dff7e0c66529
onuriltan <onur.iltan@altpro.com.tr>
2018-04-23 19:44:11 +0300

    cross origin enabled for trendtopics

--- src/main/java/com/onuriltan/twitteranalyzerserver/api/trendtopic/controller/TrendTopicController.java
+++ src/main/java/com/onuriltan/twitteranalyzerserver/api/trendtopic/controller/TrendTopicController.java
@@ -2,6 +2,7 @@ package com.onuriltan.twitteranalyzerserver.api.trendtopic.controller;
 
 import com.onuriltan.twitteranalyzerserver.api.trendtopic.model.TrendTopicModel;
 import com.onuriltan.twitteranalyzerserver.api.trendtopic.service.TrendTopicService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import javax.inject.Inject;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://onuriltan.com:5000")
 public class TrendTopicController {
 
     @Inject
