999059be95f42a1f35ad6ead72c8394014dd9d60
onur.iltan <onur.iltan@altpro.com.tr>
2018-05-09 14:53:05 +0300

    stream request validation implemented

--- src/main/java/com/onuriltan/twitteranalyzerserver/api/twitterstream/controller/StreamSocketController.java
+++ src/main/java/com/onuriltan/twitteranalyzerserver/api/twitterstream/controller/StreamSocketController.java
@@ -2,19 +2,13 @@ package com.onuriltan.twitteranalyzerserver.api.twitterstream.controller;
 
 
 import com.onuriltan.twitteranalyzerserver.api.twitterstream.model.StreamRequest;
-import com.onuriltan.twitteranalyzerserver.api.twitterstream.model.TokenizedTweet;
 import com.onuriltan.twitteranalyzerserver.api.twitterstream.service.StreamSocketService;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 
 import javax.inject.Inject;
-import javax.validation.Valid;
 
 @Controller
 public class StreamSocketController {
@@ -22,31 +16,15 @@ public class StreamSocketController {
     @Inject
     private StreamSocketService streamSocketService;
 
-    @Inject
-    private SimpMessageSendingOperations webSocket;
-
 
     @MessageMapping("/manageTwitterStream")
-    public void manageStream( @Payload StreamRequest streamRequest, SimpMessageHeaderAccessor headerAccessor){
-
+    public void manageStream(@Payload StreamRequest streamRequest, SimpMessageHeaderAccessor headerAccessor) {
         String sessionId = headerAccessor.getSessionId();
-       /* if (bindingResult.hasErrors()) {
-            TokenizedTweet tokenizedTweet = new TokenizedTweet();
-            bindingResult.getAllErrors().forEach(error -> {
-                tokenizedTweet.setException(error.toString());
-                webSocket.convertAndSendToUser(sessionId, "/queue/fetchTwitterStream", tokenizedTweet , createHeaders(sessionId));
 
-            });
+        if (streamRequest.getMessage() != null && !streamRequest.getMessage().isEmpty() && streamRequest.getMessage().length() >= 4) {
+            streamSocketService.manageStream(streamRequest, sessionId);
+        }
 
-        }*/
-        streamSocketService.manageStream(streamRequest, sessionId);
-    }
-
-    private MessageHeaders createHeaders(String sessionId) { // TODO : Make this method static bcs it is used mult places
-        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
-        headerAccessor.setSessionId(sessionId);
-        headerAccessor.setLeaveMutable(true);
-        return headerAccessor.getMessageHeaders();
     }
 
 
--- src/main/java/com/onuriltan/twitteranalyzerserver/base/BaseTwitterStream.java
+++ src/main/java/com/onuriltan/twitteranalyzerserver/base/BaseTwitterStream.java
@@ -13,7 +13,6 @@ import twitter4j.*;
 
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
